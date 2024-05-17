ad05f3a3a5226b8cc71a7560ba2fdd58316fa3ec
Cristopher <caescalante@ccoo.es>
2024-05-17 13:40:12 +0200

    añadidos usuarios

--- src/main/resources/SQLs/usersAdded.sql
+++ src/main/resources/SQLs/usersAdded.sql
@@ -1,3 +1,9 @@
 
 INSERT INTO user_pokemon (id,nombre,contrasena,email) VALUES (1,'cris','$2a$10$JJqUOVcvBvvhjnsL3TmQtuS5BZI8Xh.DpaKzCk20SwqhRY5PxTv5a','cris@cris.com');
 INSERT INTO user_roles (id,nombre) VALUES (1,'USER');
+
+INSERT INTO user_pokemon (id,nombre,contrasena,email) VALUES (2,'leo','$2a$10$mD5GV3iSuAr5kltuzXekjOHpiuyFUNzO8oC67iVC.Elh3NeEqyZbi','leo@leo.com');
+INSERT INTO user_roles (id,nombre) VALUES (2,'USER');
+
+INSERT INTO user_pokemon (id,nombre,contrasena,email) VALUES (3,'galle','$2a$10$AwdKKKBxxgo6AiZKE8RGrOLEWkUr21yAOYTmYjs.2aLW8.MjqQm3G','galle@galle.com');
+INSERT INTO user_roles (id,nombre) VALUES (3,'USER');
