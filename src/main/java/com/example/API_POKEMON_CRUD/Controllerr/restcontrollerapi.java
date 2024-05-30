0c5a4da18279f307bdf0689fce4346395e8655c7
Cristopher <caescalante@ccoo.es>
2024-05-30 16:17:11 +0200

    ssl

--- src/main/java/com/example/API_POKEMON_CRUD/Controllerr/restcontrollerapi.java
+++ src/main/java/com/example/API_POKEMON_CRUD/Controllerr/restcontrollerapi.java
@@ -25,7 +25,7 @@ import com.example.API_POKEMON_CRUD.Servicios.user_method;
 @Slf4j
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://pokecollector-p0k3.web.app/")
 public class restcontrollerapi {
 
 	@Autowired
